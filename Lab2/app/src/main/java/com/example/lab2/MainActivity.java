63a32aaad38801bf3dd698f1e8fb35badd1d3688
DimaKudelich <dima.kudelich2@mail.ru>
2020-12-16 19:17:33 +0300

    Solve problem with color

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+    Button button;
+
+    int sum;
+    int firstNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        firstNumber = Integer.parseInt(textView.getText().toString());
+        textView.setText(String.valueOf(firstNumber));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                editText = findViewById(R.id.number);
+
+                int number = Integer.parseInt(editText.getText().toString());
+                sum = number + firstNumber;
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("number", sum);
+                startActivityForResult(intent, 10);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if (requestCode == 10){
+            if (resultCode == Activity.RESULT_OK){
+                int res = data.getIntExtra("result",0);
+                textView.setText(String.valueOf(res));
+                firstNumber = Integer.parseInt(textView.getText().toString());
+            }
+        }
     }
 }
--- Lab2/app/src/main/java/com/example/lab2/SecondActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/SecondActivity.java
@@ -1,13 +1,41 @@
 package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+    Button button;
 
+    int sum;
+    int firstNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_main);
+
+        firstNum = getIntent().getIntExtra("number", 0);
+        textView = findViewById(R.id.textView);
+        textView.setText(String.valueOf(firstNum));
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                editText = findViewById(R.id.number);
+                int number = Integer.parseInt(editText.getText().toString());
+                sum = number + firstNum;
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("result", sum);
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+        });
     }
 }
